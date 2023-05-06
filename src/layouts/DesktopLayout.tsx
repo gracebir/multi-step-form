2408fd6d6db8005087c352245ace7a6bb62844a1
gracebir <grace.devolop@gmail.com>
2023-05-06 23:09:59 +0200

    layouts page

---
+++ src/layouts/DesktopLayout.tsx
@@ -0,0 +1,11 @@
+import Layout from "../Layout"
+
+const DeskTopLayout = () => {
+    return (
+        <Layout>
+            <div>jsjs</div>
+        </Layout>
+    )
+}
+
+export default DeskTopLayout
