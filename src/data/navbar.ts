40ca504a94a98254e37449f7de894bbaeb484d02
gracebir <grace.devolop@gmail.com>
2023-05-06 23:09:26 +0200

    data and layouts

---
+++ src/data/navbar.ts
@@ -0,0 +1,26 @@
+export const navdatas = [
+    {
+        id: 1,
+        title: "step 1",
+        desc: "your info",
+        link: '/'
+    },
+    {
+        id: 2,
+        title: "step 2",
+        desc: "select plan",
+        link: '/'
+    },
+    {
+        id: 3,
+        title: "step 3",
+        desc: "add-ons",
+        link: '/'
+    },
+    {
+        id: 4,
+        title: "step 4",
+        desc: "summary",
+        link: '/'
+    }
+]
