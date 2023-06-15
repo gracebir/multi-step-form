7b09e71897be11aece8ffe969a48396737c1cd4c
gracebir <grace.devolop@gmail.com>
2023-06-15 12:40:21 +0200

    mobile and desktop screen

---
+++ src/screens/Desktop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Desktop() {
+  return (
+    <div className='hidden lg:flex w-full h-screen bg-marine-blue text-color-white'>
+      <h1 >desktop</h1>
+    </div>
+  )
+}
+
+export default Desktop
---
+++ src/screens/Mobile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mobile() {
+  return (
+    <div className="block h-screen lg:hidden w-full bg-marine-blue font-ubuntu">
+      <h1 className='text-2xl text-white'>Mobile</h1>
+    </div>
+  )
+}
+
+export default Mobile
