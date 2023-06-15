8ebf7d165801b6813a3163d39c1b44504bc41622
gracebir <grace.devolop@gmail.com>
2023-06-15 18:52:42 +0200

    mobile mobile home page layout

--- src/screens/Mobile.jsx
+++ src/screens/Mobile.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import MobileLayout from '../Layouts/MobileLayout'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Mobile/Home'
 
 function Mobile() {
   return (
-    <div className="block h-screen lg:hidden w-full bg-marine-blue font-ubuntu">
-      <h1 className='text-2xl text-white'>Mobile</h1>
-    </div>
+    <MobileLayout>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </MobileLayout>
   )
 }
 
