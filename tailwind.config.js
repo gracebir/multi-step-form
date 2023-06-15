9256b83336e604a5a9cb68b965e4ce8ad5ed3a7c
gracebir <grace.devolop@gmail.com>
2023-06-15 12:27:39 +0200

    configure colors values

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'ubuntu': ['Ubuntu', 'sans-serif']
+      },
+      colors:{
+        'marine-blue': 'var(--color-marine-blue)',
+        'purplish-blue': 'var(--color-purplish-blue)',
+        'pastel-blue': 'var(--color-pastel-blue)',
+        'light-blue': 'var(--color-light-blue)',
+        'strawberry-red': 'var(--color-strawberry-red)',
+        'cool-gray': 'var(--color-cool-gray)',
+        'magnolia': 'var(--color-magnolia)',
+        'alabaster': 'var(--color-alabaster)',
+        'light-blue': 'var(--color-light-blue)',
+        'color-white': 'var(--color-white)'
+      }
+    },
   },
   plugins: [],
 }
