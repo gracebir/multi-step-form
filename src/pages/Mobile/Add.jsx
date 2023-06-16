771d29e5ecc101112dcd258f71613194977594ef
gracebir <grace.devolop@gmail.com>
2023-06-16 20:53:59 +0200

    Add ons page mobile view

--- src/pages/Mobile/Add.jsx
+++ src/pages/Mobile/Add.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { addOns } from '../../data'
+import CardAdd from '../../components/CardAdd'
 
 function Add() {
   return (
-    <div>
-      Add
+    <div className='flex flex-col gap-6'>
+       <div className='flex flex-col gap-1'>
+        <h1 className='text-2xl text-marine-blue font-bold'>Pick add-ons</h1>
+        <p className="text-cool-gray text-base">Add-ons help enhance your gaming experience.</p>
+      </div>
+      <div className='flex flex-col gap-3'>
+        {addOns.map((item)=> (
+          <CardAdd
+          key={item.id}
+          service={item.service}
+          desc={item.desc}
+          month={item.month}
+          year={item.year}
+          />
+        ))}
+      </div>
     </div>
   )
 }
