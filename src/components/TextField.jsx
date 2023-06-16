36b2ad64e873c28f77787616e117507834093ae7
gracebir <grace.devolop@gmail.com>
2023-06-16 12:41:36 +0200

    handle error handle to textfield

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-function TextField({placeholder, type, name, onChange}) {
-  return (
-    <div className='flex flex-col gap-1'>
-      <label className='text-marine-blue font-medium'>{name}</label>
-      <input className='py-3 border border-light-gray px-4 w-full rounded-lg outline-none text-marine-blue placeholder:text-cool-gray text-lg' type={type} placeholder={placeholder} onChange={onChange}/>
-    </div>
-  )
+function TextField({ placeholder, type, value, onBlur, name,label, isError=false, onChange }) {
+    return (
+        <div className='flex flex-col gap-1'>
+            <div className='flex justify-between items-center'>
+                <label className='text-marine-blue font-medium'>{label}</label>
+                {isError && (<span className='text-strawberry-red text-sm font-semibold'>{"This Field is Required"}</span>)}
+            </div>
+            <input name={name} value={value} onBlur={onBlur} className={`py-3 border ${isError ? 'border-strawberry-red':'border-light-gray hover:border-purplish-blue'}  px-4 w-full rounded-lg outline-none text-marine-blue placeholder:text-cool-gray text-lg`} type={type} placeholder={placeholder} onChange={onChange} />
+        </div>
+    )
 }
 
 export default TextField
