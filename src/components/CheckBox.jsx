c48602992343bbd1d1af936ca7b539e47b7e2693
gracebir <grace.devolop@gmail.com>
2023-06-16 20:52:03 +0200

    checkbox logic

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import check from '../assets/icon-checkmark.svg'
 
-function CheckBox() {
-    const [isChecked, setIsChecked] = useState(false)
+function CheckBox({isChecked, setIsChecked}) {
     const toggle = () => setIsChecked(!isChecked)
   return (
     <div onClick={toggle} className={`border border-cool-gray w-5 h-5 cursor-pointer rounded-md ${isChecked && "bg-purplish-blue"} flex items-center justify-center`}>
